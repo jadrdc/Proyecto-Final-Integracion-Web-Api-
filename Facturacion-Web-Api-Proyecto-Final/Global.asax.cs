6c5cd3ca7e26e736640994f35184c7bb89dae467
jadrdc <jadrdc@gmail.com>
2017-03-04 23:22:43 -0400

    Proyecto Base Web Api para Integracion Propietaria

---
+++ Facturacion-Web-Api-Proyecto-Final/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Facturacion_Web_Api_Proyecto_Final
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
